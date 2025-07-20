62751a193fd2e5fe72635264c31b8d4f278e7bc2
jack-hoon <lHDlLucy@gmail.com>
2025-07-20 13:01:48 +0900

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,15 +1,11 @@
 /** @type {import('next').NextConfig} */
 
-// @ts-check
-import { PHASE_DEVELOPMENT_SERVER } from 'next/constants'
- 
-export default (phase) => {
-  const isProd = process.env.NODE_ENV === 'production'
-  /**
-   * @type {import('next').NextConfig}
-   */
-  const nextConfig = {
-    assetPrefix: isProd ? 'https://jack-hoon.github.io/jackhoon-portfolio/' : undefined,
+const prefix =
+  process.env.NODE_ENV === 'production' ? 'https://jack-hoon.github.io/jackhoon-portfolio/' : ''
+
+const nextConfig = {
+  output: 'export',
+  assetPrefix: prefix,
   }
-  return nextConfig
-}
+
+export default nextConfig
