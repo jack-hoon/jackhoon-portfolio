bad2d47bf57089333d36a5d3f2c3e923cf83283c
jack-hoon <lHDlLucy@gmail.com>
2025-07-20 12:46:05 +0900

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
+
+const prefix = process.env.NODE_ENV === 'production' ? 'https://jack-hoon.github.io/jackhoon-portfolio/' : ''
+
 const nextConfig = {
-  output: 'export'
+  output: 'export',
+  assetPrefix: prefix
 };
 
 export default nextConfig;
