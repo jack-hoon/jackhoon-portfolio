a0756c99820dc433491408158e5a1406ded155e4
jack-hoon <lHDlLucy@gmail.com>
2025-07-20 14:12:46 +0900

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,15 @@
 /** @type {import('next').NextConfig} */
 
 const prefix =
-  process.env.NODE_ENV === 'production' ? '/jackhoon-portfolio/' : ''
+  process.env.NODE_ENV === 'production' ? '/jackhoon-portfolio/' : '';
+
+const basePath =
+  process.env.NODE_ENV === 'production' ? '/jackhoon-portfolio' : '';
 
 const nextConfig = {
   output: 'export',
   assetPrefix: prefix,
-  }
+  basePath: basePath
+}
 
 export default nextConfig
